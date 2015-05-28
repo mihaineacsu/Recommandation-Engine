1a9117dafd409cd9e618c340ee2e505ae68fd823
Ana Badescu <anabee.emacs@gmail.com>
2015-05-28 11:37:51 +0300

    Latest at 11am

--- data_processing/correlation.py
+++ data_processing/correlation.py
@@ -12,29 +12,64 @@ if len(argv) is 2:
 client = MongoClient()
 db = client.yelp
 similarity_threshold = 0
+test_mode = True
+test_user_businesses = {}
 
 def k_recommandations(user_id, k=10):
 	recommandations = compute_recommandations(user_id)
 	sorted_business_ids = [pair for pair in sorted(recommandations.items(), key=itemgetter(1))]
 	sorted_business_ids.reverse()
 
-	print "----> Mean squared err: %s\n" % compute_mean_sq_err(sorted_business_ids)
+	mae = compute_mean_sq_err(sorted_business_ids)
+	if mae is not None:
+		rmse = sqrt(mae)
+	else:
+		rmse = None
+	accuracy = compute_accuracy(sorted_business_ids)
+
+	if test_mode is True:
+		print "----> Mean squared err: %s\n" % mae
+		print "----> RMSE err: %s\n" % rmse
+		print "----> Accuracy : %s\n" % accuracy
 
 	return [] if not sorted_business_ids else sorted_business_ids[:k]
 
 def compute_mean_sq_err(sorted_business_ids):
+
 	initial_scores = []
+	optained_scores = []
 	for i in sorted_business_ids:
-		found = db['business'].find_one({'business_id': i[0]})
-		if not found:
-			raise Exception('Not found!')
-		initial_scores.append(found['stars'])
+		if i[0] in test_user_businesses:
+			initial_scores.append(test_user_businesses[i[0]]['stars'])
+			optained_scores.append(round(i[1]))
 
 	if len(initial_scores) is 0:
 		print "not enough data"
 		return None
 
-	return mean_squared_error(initial_scores, [i[1] for i in sorted_business_ids])
+	print initial_scores
+	print optained_scores
+	return mean_squared_error(initial_scores, [i for i in optained_scores])
+
+def compute_accuracy(sorted_business_ids):
+
+	initial_scores = []
+	optained_scores = []
+	for i in sorted_business_ids:
+		if i[0] in test_user_businesses:
+			initial_scores.append(test_user_businesses[i[0]]['stars'])
+			optained_scores.append(round(i[1]))
+
+	if len(initial_scores) is 0:
+		print "not enough data"
+		return None
+
+	counter = 0
+	for i in range(len(initial_scores)):
+		if initial_scores[i] == optained_scores[i]:
+			counter+=1
+
+	return counter/float(len(initial_scores))
 
 def compute_recommandations(user_id):
 	""" Returns a dictionary with a mapping of business_id : score.
@@ -43,7 +78,17 @@ def compute_recommandations(user_id):
 	weighted_scores = {}
 	normal_scores = {}
 
-	current_user_businesses = reviewed_businesses(user_id)
+
+	if test_mode is True:
+		all_user_businesses = reviewed_businesses(user_id)
+		no_businesses = len(all_user_businesses)
+		train_size = no_businesses*7/10
+		current_user_businesses = get_n_businesses(all_user_businesses, train_size)
+		print train_size
+		print no_businesses - train_size
+	else:
+		current_user_businesses = reviewed_businesses(user_id)
+	
 
 	for u in db['users'].find().limit(upper_limit):
 		other_user_id = u['user_id']
@@ -51,13 +96,18 @@ def compute_recommandations(user_id):
 		if other_user_id == user_id:
 			continue
 
+
 		other_user_businesses = reviewed_businesses(other_user_id)
 		
-		similarity_score = pearson_correlation(current_user_businesses, other_user_businesses)
+		similarity_score = compute_similarity(current_user_businesses, other_user_businesses)
 		if similarity_score <= similarity_threshold:
 			continue
 
-		recommandations = list_recommandations(current_user_businesses, other_user_businesses)
+		if test_mode is True:
+			recommandations = other_user_businesses
+		else:
+			recommandations = list_recommandations(current_user_businesses, other_user_businesses)
+		
 		for recommended_business in recommandations:
 			weighted_scores.setdefault(recommended_business, 0)
 			normal_scores.setdefault(recommended_business, 0)
@@ -68,6 +118,20 @@ def compute_recommandations(user_id):
 
 
 	return dict((business_id, float(weighted_scores[business_id] / normal_scores[business_id])) for business_id in weighted_scores)
+
+def get_n_businesses(all_user_businesses, n):
+	
+	count = 0
+	train_user_businesses = {}
+	for business in all_user_businesses: 
+		if count < n:
+			train_user_businesses[business] = all_user_businesses.get(business)
+			count += 1
+		else:
+			test_user_businesses[business] = all_user_businesses.get(business)
+
+
+	return train_user_businesses
 
 
 def reviewed_businesses(id):
@@ -101,19 +165,17 @@ def intersect(a, b):
 
 	return [item for item in a if item in b]
 
-def pearson_correlation(businesses_person1, businesses_person2):
+
+def compute_similarity(businesses_person1, businesses_person2):
 	common_businesses = intersect(businesses_person1, businesses_person2) 
 
-	# if len(common_businesses) >= 1:
-	# 	print 'common business:'
-	# 	print common_businesses
 	if not common_businesses:
 		return 0
 
 	# return cosineDistance(businesses_person1, businesses_person2, common_businesses)
-	return classic(businesses_person1, businesses_person2, common_businesses)
+	return pearson_correlation(businesses_person1, businesses_person2, common_businesses)
 
-def classic(businesses_person1, businesses_person2, common_businesses):
+def pearson_correlation(businesses_person1, businesses_person2, common_businesses):
 	sum_person1 = sum([businesses_person1[business_id]['stars'] for business_id in common_businesses])
 	sum_person2 = sum([businesses_person2[business_id]['stars'] for business_id in common_businesses])
 
@@ -145,9 +207,15 @@ def cosineDistance(businesses_person1, businesses_person2, common_businesses):
 	return product_sum/float(denominator)
 
 
+def similarity_normalizer (similarity, no_common):
+	""" Tries to  normalize the similarity when there are few user that recommended
+		that business
+	"""
+	constant = 1.5
+	return similarity*no_common/float(no_common+constant)
 
 def pearson_correlation_old(id_person1="Xqd0DzHaiyRqVH3WRG7hzg", id_person2="H1kH6QZV7Le4zqTRNxoZow"):
-	reviews_person1 = db['review'].find({'user_id': id_person1})
+	reviews_person1 = db['review'].find({'user_id': id_person1, 'city':"Pithsburg"})
 	reviews_person2 = db['review'].find({'user_id': id_person2})
 
 	businesses_person1 = {}
@@ -187,4 +255,4 @@ def pearson_correlation_old(id_person1="Xqd0DzHaiyRqVH3WRG7hzg", id_person2="H1k
 	return nominator / denominator
 
 
-print k_recommandations("ZWOj6LmzwGvMDh-A85EOtA")
+print k_recommandations("rpOyqD_893cqmDAtJLbdog")
